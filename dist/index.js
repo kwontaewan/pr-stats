c04387fbe0022c8c631009393353ef27c0122de8
Gunter <xc200925@gmail.com>
2025-01-14 12:50:35 +0900

    Update time-from-review-to-merge.ts

--- dist/index.js
+++ dist/index.js
@@ -16593,13 +16593,13 @@ const util_1 = __nccwpck_require__(9604);
 const timeFromReviewToMerge = (pr) => {
     const { timeline } = pr;
     const mergedAtNum = Number(timeline.mergedAt);
-    const firstRequestedAtNum = Number(timeline.firstRequestedAt);
+    const firstRequestedAtNum = Number(timeline.createdAt);
     // 만약 숫자로 변환이 불가능하다면(NaN),
     // 어떤 값이 잘못되었는지 프린트(메시지)해준다.
     if (Number.isNaN(mergedAtNum) || Number.isNaN(firstRequestedAtNum)) {
         return {
             value: 0,
-            message: `값이 올바르지 않습니다. (mergedAt: ${timeline.mergedAt}, firstRequestedAt: ${timeline.firstRequestedAt})`
+            message: `값이 올바르지 않습니다. (mergedAt: ${timeline.mergedAt}, firstRequestedAt: ${timeline.createdAt})`
         };
     }
     const value = mergedAtNum - firstRequestedAtNum;
--- src/stats/pr/time-from-review-to-merge.ts
+++ src/stats/pr/time-from-review-to-merge.ts
@@ -6,14 +6,14 @@ export const timeFromReviewToMerge = (pr: PR): IResult<number> => {
   const { timeline } = pr;
 
   const mergedAtNum = Number(timeline.mergedAt);
-  const firstRequestedAtNum = Number(timeline.firstRequestedAt);
+  const firstRequestedAtNum = Number(timeline.createdAt);
 
   // 만약 숫자로 변환이 불가능하다면(NaN),
   // 어떤 값이 잘못되었는지 프린트(메시지)해준다.
   if (Number.isNaN(mergedAtNum) || Number.isNaN(firstRequestedAtNum)) {
     return {
       value: 0,
-      message: `값이 올바르지 않습니다. (mergedAt: ${timeline.mergedAt}, firstRequestedAt: ${timeline.firstRequestedAt})`
+      message: `값이 올바르지 않습니다. (mergedAt: ${timeline.mergedAt}, firstRequestedAt: ${timeline.createdAt})`
     };
   }
 
